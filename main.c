c6cb6ce7a53b0c16fa0eff9d262c2a0f703c9cdc
Leon <OfficialMuffin@protonmail.com>
2018-09-06 15:59:02 +0100

    Added Cond. Operators & Switch Statements

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 /*
 This is multi Line Comment
-Made by OfficialMuffin
+Made by Leon Whelan
 */
 
 int main()
@@ -63,7 +63,7 @@ int main()
     printf("The sum of 2 and 2 is: ", sum);
 
 
-        //If Condition
+    //If Condition
     puts(" \n-------------------------- ");
     puts("|**************************|");
     puts("|*******IF CONDITION*******|");
@@ -79,6 +79,49 @@ int main()
     } else {
         printf("You are under 18! You cannot continue!");
     }
+
+
+    //Conditional Operators
+    puts(" \n--------------------------------- ");
+    puts("|***********************************|");
+    puts("|*******CONDITIONAL OPERATORS*******|");
+    puts("|***********************************|");
+    puts(" -----------------------------------\n ");
+
+    int num1 = 100, num2 = 500;
+    int num3;
+
+    //(expression) ? (if true) : (if false)
+    num3 = (num1 > num2) ? num2 : num1;
+    //if num1 is greater than num2 ? if true return num2 : if false return num1
+    printf("Answer = %d",num3);
+
+    //Switch Statements
+    puts(" \n--------------------------------- ");
+    puts("|***********************************|");
+    puts("|*********SWITCH STATEMENTS*********|");
+    puts("|***********************************|");
+    puts(" -----------------------------------\n ");
+
+    int marks = 80; //Initialise variable
+
+    switch(marks) //Whatever we are trying to measure
+    {
+        case 80 : //if marks == 80
+            printf("You have passed the module!");
+            break;
+        case 70 : //if marks == 70
+            printf("You have not satisfied the grade boundaries to pass the module!");
+            break;
+        case 50 :
+            printf("You will need to resit the test to pass the module!");
+            break;
+        default : //This is like an ELSE statement
+            printf("Grade Unavailable...");
+    }
+
+
+
 
     //Leave this line below as it is to end program
     return 0;
